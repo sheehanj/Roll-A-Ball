028e46016c39790d4043aee3ac8a27d80e01bfe6
sheehanj <sheedge01@gmail.com>
2018-04-20 15:08:24 -0400

    Initial Version

---
+++ Roll a Ball/Assets/Scripts/HoleCollision.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoleCollision : MonoBehaviour
+{
+    private static int count;
+
+    public Text countText;
+    public Text winText;
+    public Text loseText;
+
+    // Use this for initialization
+    void Start ()
+    {
+        count = 0;
+        UpdateCountText();
+        winText.text = "";
+        loseText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.SetActive(false);
+            loseText.text = "Scratch! You Lose!";
+        }
+
+        else if(other.gameObject.CompareTag("Ball"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            UpdateCountText();
+        }
+    }
+
+    void UpdateCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 6)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
---
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    
+
+    public float speed;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+}
